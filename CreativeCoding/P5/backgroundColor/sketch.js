e50105b7913a6462c2b9fbe2ae3bd40135235221
victoriabrowder <vbrowder18@gmail.com>
2024-01-04 14:54:07 -0500

    background

---
+++ CreativeCoding/P5/backgroundColor/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas (windowWidth, windowHeight); //creating window and height to be size of browser
+  background (255,0,0); //red background
+}
+
+function draw() {
+}
