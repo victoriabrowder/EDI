db807f230a10cc857d8c08cdcaa0a375cfd8c437
victoriabrowder <vbrowder18@gmail.com>
2024-01-04 18:33:38 -0500

    2x2 loop

---
+++ CreativeCoding/P5/2x2_loop/sketch.js
@@ -0,0 +1,20 @@
+function setup() { //runs once
+  createCanvas(720, 720);
+  fill('red');
+  strokeWeight(8);
+}
+
+function draw() {
+  background(220);
+  
+  translate (60, 60);
+  
+  for(x=0; )
+  
+  quad(0,0,
+      300,0,
+      300,300,
+      0, 300);
+  
+  noLoop ();
+}
