310b3da9f36e3b452d72b190e1e2da551fbb2c4e
victoriabrowder <vbrowder18@gmail.com>
2024-01-04 18:05:21 -0500

    2x2

---
+++ CreativeCoding/P5/2x2/sketch.js
@@ -0,0 +1,33 @@
+function setup() { //runs once
+  createCanvas(720, 720);
+  fill('red');
+  strokeWeight(8);
+}
+
+function draw() {
+  background(220);
+  
+  translate (60, 60);
+  
+  quad(0,0,
+      300,0,
+      300,300,
+      0, 300);
+  
+  quad(300,0,
+      600,0,
+      600,300,
+      300, 300);//increase x 300 pixels
+  
+  quad(0,300,
+      300,300,
+      300,600,
+      0, 600);//increase y 300 pixels
+  
+  quad(300,300,
+      600,300,
+      600,600,
+      300, 600);//increase y 300 pixels
+  
+  noLoop ();
+}
